82800ba1a5dadbfcbe0a97f0dd728e2ac8b1fee2
tallyhawley <trsileneh@gmail.com>
2017-07-11 14:29:40 -0700

    JULY 11 2017 PART 2

--- src/boomboom/Dino.java
+++ src/boomboom/Dino.java
@@ -1,5 +1,7 @@
 package boomboom;
 
+import java.util.InputMismatchException;
+
 public class Dino {
 	static String dinoAct;
 	static int dinoLine;
@@ -13,15 +15,11 @@ public class Dino {
 	
 	static private void checkTime(Player player){
 		System.out.println("\nthe clock reads " + Player.bombTime + ".");
-		player.subtractMoves();
-		player.setTime();
 	}
 	
 	static private void defaultChoice(Player player){
 		System.out.println("that's not an option right now. be more careful -\n"
 				+ "you're losing time.");
-		player.subtractMoves();
-		player.setTime();
 	}
 	
 	static void dinoChallenge(Player player){
@@ -34,23 +32,28 @@ public class Dino {
 				System.out.println("\n\"what are you doing, kid?\" you ask, voice soft.\n"
 						+ "he looks at you, expression turning almost sad. \"get out of here,\" he says,\n"
 						+ "quieter.");
-				defaultMoves(player);
 				while(!Player.gameOver){
+					defaultMoves(player);
 					switch(dinoAct){
 						case "t": 
 							//TODO PATH TALK-TALK
 							System.out.println("1: \"i'm not leaving until you disable that.\"\n"
 											+ "2: \"i'm just here to help.\"\n"
 											+ "3: \"don't do this.\"");
+							try{
 							dinoLine = Game.scan.nextInt();
+							}catch(InputMismatchException e){
+								System.out.println("that's not an option. be careful - you're running out of time.");
+								break;
+							}
 							if(dinoLine == 1){
 								//TODO PATH TALK-TALK-1
 								System.out.println("\"i'm not leaving until you disable that,\" you say.\n"
 										+ "the kids smiles the empty smile of a man sentenced to death.\n"
 										+ "\"if you don't leave now, you won't have a chance to leave\n"
 										+ "later,\" he says, cryptic.");
-								defaultMoves(player);
 								while(!Player.gameOver){
+									defaultMoves(player);
 									switch(dinoAct){
 										case "t":
 										//TODO PATH TALK-TALK-1-TALK
@@ -100,13 +103,24 @@ public class Dino {
 									}
 								}
 							}
-							//TODO PATH TALK-TALK-2
 							else if(dinoLine == 2){
-								
+							//TODO PATH TALK-TALK-2	
+								System.out.println("\"i'm just here to help,\" you say gently.\n"
+										+ "\"i don't need help,\" he spits. \"just leave.\"\n"
+										+ "venom laces his words. perhaps it's spite at your request to help.\n"
+										+ "you don't know.");
+								defaultMoves(player);
+							}
+							else if(dinoLine == 3){
+							//TODO PATH TALK-TALK-3
+							}
+							else{
+								System.out.println("that's not an option. be careful - you're running out of time.");
+								break;
 							}
 						break;
-						//TODO PATH 1-2
 						case "i":
+						//TODO PATH TALK-ITEM
 							Action.printInv(player);
 							int i;
 							if(player.inventory.isEmpty()){
@@ -127,8 +141,8 @@ public class Dino {
 										+ "click as it touches the floor, and the boy zeroes in on it immediately,\n"
 										+ "eyes narrowing in suspicion.\n"
 										+ "\"who else knows i'm here?\" he asks, wary.");
-								defaultMoves(player);
 								while(!Player.gameOver){
+									defaultMoves(player);
 									switch(dinoAct){
 										case "t":
 										case "i":
@@ -144,14 +158,13 @@ public class Dino {
 			 					
 			 				}
 							break;
-						//TODO CLOCK && DEFAULT
 						case "c": checkTime(player); break;
 						default: defaultChoice(player); break;
 					}
 				}
 				break;
-			//TODO PATH 2
 			case "i":
+			//TODO PATH ITEM
 				Action.printInv(player);
 				int i;
 				if(player.inventory.isEmpty()){
@@ -167,6 +180,7 @@ public class Dino {
 					System.out.println("that's not an option right now. be more careful -\nyou're losing time.");
 				}
 				else if(player.inventory.get(Integer.parseInt(dinoAct)).equals(Player.radio)){
+				//TODO PATH ITEM-RADIO
 					System.out.println("you pull the radio out of your pocket and lower it to the floor behind you,\n"
 							+ "sliding the cord delicately between your fingers. it makes a little\n"
 							+ "click as it touches the floor, and the boy zeroes in on it immediately,\n"
@@ -176,7 +190,9 @@ public class Dino {
 					while(!Player.gameOver){
 						switch(dinoAct){
 							case "t":
+							//TODO PATH ITEM-RADIO-TALK
 							case "i":
+							//TODO PATH ITEM-RADIO-ITEM
 							case "c": checkTime(player); break;
 							default: defaultChoice(player); break;
 						}
@@ -189,7 +205,6 @@ public class Dino {
  					
  				}
 				break;
-			//TODO CLOCK && DEFAULT
 			case "c": checkTime(player); break;
 			default: defaultChoice(player); break; 
 		}
